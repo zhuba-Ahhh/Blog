9e27bc59ad2a5d322c8c2ad9b0053a81a7bbea6d
zhuba-Ahhh <3477826311@qq.com>
2023-09-07 20:37:54 +0800

    docs: :memo: add some

--- site.config.ts
+++ site.config.ts
@@ -34,6 +34,26 @@ export default defineSiteConfig({
 
   search: {
     enable: true,
+    type: 'fuse',
+  },
+
+  fuse: {
+    options: {
+      keys: ['title', 'tags', 'categories', 'excerpt', 'content'],
+      /**
+       * @default 0.6
+       * @see https://www.fusejs.io/api/options.html#threshold
+       * 设置匹配阈值，越低越精确
+       */
+      // threshold: 0.6,
+      /**
+       * @default false
+       * @see https://www.fusejs.io/api/options.html#ignoreLocation
+       * 忽略位置
+       * 这对于搜索文档全文内容有用，若无需全文搜索，则无需设置此项
+       */
+      ignoreLocation: true,
+    },
   },
 
   sponsor: {
